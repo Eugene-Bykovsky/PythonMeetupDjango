48d9c2138afd4fb667504f478398d70d2705ea29
TonyCleem <tonycleem@gmail.com>
2024-11-28 23:23:27 +0700

    add model Speaker

--- pythonmeetup/api/models.py
+++ pythonmeetup/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Speaker(models.Model):
+	full_name = models.CharField(max_lenght=150)
+	tg_id = model.CharField(max_lenght, unique=True)
+	tech_stack = models.TextField()
+
+	def __str__(self):
+		return self.full_name
